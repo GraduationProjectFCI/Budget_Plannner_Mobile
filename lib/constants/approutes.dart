562deeef9c2b4d4eafd4fb0fd6d0d7572ad5c4a9
Abdallah-Abdalmonem <bedo30176@gmail.com>
2023-04-26 11:20:06 +0200

    App routes constant

---
+++ lib/constants/approutes.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../view/screens/confirm_screen.dart';
+import '../view/screens/deadlines_screen.dart';
+import '../view/screens/login_screen.dart';
+import '../view/screens/profile_screen.dart';
+import '../view/screens/register_screen.dart';
+import '../view/screens/sheets_screen.dart';
+
+class AppRoutes {
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String sheets = '/sheets';
+  static const String profile = '/profile';
+  static const String confirmation = '/confirmation';
+  static const String deadlines = '/sheets';
+}
+
+Map<String, Widget Function(BuildContext)> routes = {
+  AppRoutes.login: (context) => LoginScreen(),
+  AppRoutes.register: (context) => RegisterScreen(),
+  AppRoutes.confirmation: (context) => ConfirmScreen(),
+  AppRoutes.sheets: (context) => SheetsScreen(),
+  AppRoutes.profile: (context) => ProfileScreen(),
+  AppRoutes.deadlines: (context) => DeadlinesScreen(),
+};
