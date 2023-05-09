f87aa0010a6b2019944499fc04663c935bb90783
Abdallah-Abdalmonem <bedo30176@gmail.com>
2023-05-09 20:32:10 +0300

    Refactor for register model

--- lib/models/register_model.dart
+++ lib/models/register_model.dart
@@ -1,18 +1,11 @@
 class RegisterModel {
-  int? status;
   var msg;
   String? userId;
-  RegisterModel({this.status, this.msg, this.userId});
+
+  // RegisterModel({this.msg, this.userId});
 
   RegisterModel.fromJson(Map<String, dynamic> json) {
-    status = json['status'];
-    msg = json['msg'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['status'] = this.status;
-    data['msg'] = this.msg;
-    return data;
+    msg = json["msg"];
+    userId = json["user_id"];
   }
 }
